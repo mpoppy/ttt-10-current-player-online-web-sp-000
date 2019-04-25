c392235337b905157fb39d39a1cb34394100a71c
Marc Poppy <poppy.marc@gmail.com>
2019-04-25 05:06:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -15,5 +15,5 @@ def current_player(board)
 #use turn count to determine if it's x or o
 # if even it is X, if it has remainder 0
 # if odd it is O, if it has remainder 1
-  turn_count(board) == 2 ? "X" : "O"
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
